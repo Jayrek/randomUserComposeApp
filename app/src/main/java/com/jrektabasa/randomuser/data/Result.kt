b761ab042f07cad6c694050d1282d2cb559f406b
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-11-20 21:58:29 +0800

    create generic class Result.kt

---
+++ app/src/main/java/com/jrektabasa/randomuser/data/Result.kt
@@ -0,0 +1,12 @@
+package com.jrektabasa.randomuser.data
+
+import java.lang.Exception
+
+sealed class Result<out R> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+}
+
+fun <T> Result<T>.successOr(fallback: T): T {
+    return (this as? Result.Success<T>)?.data ?: fallback
+}
