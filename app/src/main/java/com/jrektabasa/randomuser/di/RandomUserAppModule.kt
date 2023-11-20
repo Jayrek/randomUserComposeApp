bcc05072f23335e18b15d7f593c90a6e84a01fb4
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-11-20 21:59:40 +0800

    provides DI for retrofit

--- app/src/main/java/com/jrektabasa/randomuser/di/RandomUserAppModule.kt
+++ app/src/main/java/com/jrektabasa/randomuser/di/RandomUserAppModule.kt
@@ -1,4 +1,27 @@
 package com.jrektabasa.randomuser.di
 
+import com.jrektabasa.randomuser.network.RandomUserInterface
+import com.jrektabasa.randomuser.ui.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RandomUserAppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): RandomUserInterface {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RandomUserInterface::class.java)
+    }
+
 }
