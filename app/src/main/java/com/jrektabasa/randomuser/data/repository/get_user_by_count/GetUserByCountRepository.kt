60faa620ad0dc6851a69d8b8208cf427c7dc8a74
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-11-20 21:58:56 +0800

    add get user by count repository and implementation

---
+++ app/src/main/java/com/jrektabasa/randomuser/data/repository/get_user_by_count/GetUserByCountRepository.kt
@@ -0,0 +1,8 @@
+package com.jrektabasa.randomuser.data.repository.get_user_by_count
+
+import com.jrektabasa.randomuser.model.RandomUserResult
+
+interface GetUserByCountRepository {
+
+    suspend fun getRandomUserByCount(result: Int): RandomUserResult
+}
