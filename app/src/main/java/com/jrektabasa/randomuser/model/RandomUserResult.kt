ef35d35956622f914b4716f4b385f84522e48d99
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-11-20 22:00:05 +0800

    create RandomUserInterface.kt class

---
+++ app/src/main/java/com/jrektabasa/randomuser/model/RandomUserResult.kt
@@ -0,0 +1,6 @@
+package com.jrektabasa.randomuser.model
+
+data class RandomUserResult(
+    val info: Info,
+    val results: List<Result>
+)
---
+++ app/src/main/java/com/jrektabasa/randomuser/network/RandomUserInterface.kt
@@ -0,0 +1,10 @@
+package com.jrektabasa.randomuser.network
+
+import com.jrektabasa.randomuser.model.RandomUserResult
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RandomUserInterface {
+    @GET("/api/")
+    suspend fun getRandomUserList(@Query("results") result: Int): RandomUserResult
+}
