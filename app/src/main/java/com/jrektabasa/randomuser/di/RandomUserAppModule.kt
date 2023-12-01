cfc04d25fc38a2821e009c4ff67ed67376b3f2c5
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-02 05:56:18 +0800

    added okhttp client and separate instance for retrofit

--- app/src/main/java/com/jrektabasa/randomuser/di/RandomUserAppModule.kt
+++ app/src/main/java/com/jrektabasa/randomuser/di/RandomUserAppModule.kt
@@ -1,11 +1,13 @@
 package com.jrektabasa.randomuser.di
 
 import com.jrektabasa.randomuser.network.RandomUserInterface
-import com.jrektabasa.randomuser.ui.utils.Constants.BASE_URL
+import com.jrektabasa.randomuser.ui.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -14,14 +16,33 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object RandomUserAppModule {
 
+    @Provides
+    fun provideBaseUrl() = Constants.BASE_URL
+
     @Singleton
     @Provides
-    fun provideRetrofit(): RandomUserInterface {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+    fun provideRetrofit(
+        baseUrl: String, okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
             .build()
-            .create(RandomUserInterface::class.java)
-    }
 
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        retrofit: Retrofit
+    ): RandomUserInterface =
+        retrofit.create(RandomUserInterface::class.java)
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY)
+            ).build()
 }
