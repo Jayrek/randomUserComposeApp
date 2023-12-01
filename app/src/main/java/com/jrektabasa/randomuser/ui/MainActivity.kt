90e36cb2a285b5ece4f60062f21765f65104068c
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-02 05:55:50 +0800

    updated the main activity for the user list display

--- app/src/main/java/com/jrektabasa/randomuser/ui/MainActivity.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -22,9 +31,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.jrektabasa.randomuser.model.UserResult
 import com.jrektabasa.randomuser.ui.screen.viewmodel.GetUserByCountViewModel
 import com.jrektabasa.randomuser.ui.theme.MyApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,9 +50,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            val viewModel: GetUserByCountViewModel = viewModel()
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -49,33 +63,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun ProfileAccountScreen(
     viewModel: GetUserByCountViewModel = hiltViewModel()
 ) {
     val user = viewModel.user.collectAsState()
-    Log.i("ProfileAccountScreen", "ProfileAccountScreen: ${user.value?.results?.get(0)?.email}")
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Column {
-            Text(text = "Email Address")
-            EmailTextField()
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(text = "Password")
-            PasswordTextField()
+    if (user.value != null) {
+        val userResults: List<UserResult> = user.value!!.results
+        LazyColumn {
+            itemsIndexed(items = userResults) { index, user ->
+                ListTileUserItem(
+                    leading = user.picture.medium,
+                    title = "${user.name.first} ${user.name.last}",
+                    subTitle = user.email,
+                ) {
+                    Log.d("ListTileUserItem", "$index. ${user.name.first}")
+                }
+            }
         }
-//        Text(text = "Jayrek Tabasa")
-//        Text(text = "Mobile Number: ")
+    }
+}
 
+@Composable
+fun ListTileUserItem(
+    leading: String,
+    title: String,
+    subTitle: String,
+    onTap: () -> Unit
+) {
+    OutlinedCard(
+        modifier = Modifier
+            .padding(all = 5.dp)
+            .fillMaxWidth()
+            .clickable(onClick = onTap),
+        shape = RoundedCornerShape(5.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp),
+        ) {
+            AsyncImage(
+                model = leading,
+                contentDescription = "user image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .border(
+                        width = 1.dp,
+                        color = Color.Black,
+                        shape = CircleShape
+                    ),
+            )
+            Column(modifier = Modifier.padding(all = 20.dp)) {
+                Text(
+                    text = title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = subTitle)
+            }
+        }
     }
 }
 
