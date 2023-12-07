6c999222c8d2a1bb6f12686e8b30cfb1cff32a98
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-07 22:45:23 +0800

    add drop down menu item

--- app/src/main/java/com/jrektabasa/randomuser/ui/MainActivity.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/MainActivity.kt
@@ -2,31 +2,46 @@ package com.jrektabasa.randomuser.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -34,6 +49,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,41 +63,113 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ProfileAccountScreen()
-                }
+                var mDisplayMenu by remember { mutableStateOf(false) }
+                var isList by remember { mutableStateOf(true) }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("RandomUser", color = Color.White) },
+                            colors = TopAppBarDefaults.smallTopAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary
+                            ),
+                            actions = {
+                                IconButton(onClick = {
+                                    mDisplayMenu = !mDisplayMenu
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Default.MoreVert,
+                                        contentDescription = null,
+                                        tint = Color.White
+                                    )
+                                }
+                                DropdownMenu(
+                                    expanded = mDisplayMenu,
+                                    onDismissRequest = { mDisplayMenu = false },
+                                    modifier = Modifier
+                                        .widthIn(min = 120.dp, max = 240.dp)
+                                        .background(MaterialTheme.colorScheme.background)
+                                ) {
+                                    ActionMenuItem(
+                                        label = "List view", onClick = {
+                                            mDisplayMenu = true
+                                            mDisplayMenu = false
+                                        }
+                                    )
+                                    ActionMenuItem(
+                                        label = "Grid view", onClick = {
+                                            isList = false
+                                            mDisplayMenu = false
+                                        }
+                                    )
+                                    ActionMenuItem(
+                                        label = "Sort by name", onClick = {
+                                            mDisplayMenu = false
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    },
+                    content = {
+                        Box(modifier = Modifier.padding(it)) {
+                            Surface(
+                                modifier = Modifier.fillMaxSize(),
+                                color = MaterialTheme.colorScheme.background
+                            ) {
+                                ProfileAccountScreen(isList = isList)
+                            }
+                        }
+                    }
+                )
+
             }
         }
     }
 }
 
 @Composable
+fun ActionMenuItem(label: String, onClick: () -> Unit) {
+    val context = LocalContext.current
+    DropdownMenuItem(text = { Text(text = label) }, onClick = {
+        onClick()
+        Toast.makeText(context, label, Toast.LENGTH_SHORT).show()
+    })
+}
+
+@Composable
 fun ProfileAccountScreen(
-    viewModel: GetUserByCountViewModel = hiltViewModel()
+    viewModel: GetUserByCountViewModel = hiltViewModel(),
+    isList: Boolean = true
 ) {
     val user = viewModel.user.collectAsState()
     if (user.value != null) {
         val userResults: List<UserResult> = user.value!!.results
-        LazyColumn {
-            itemsIndexed(items = userResults) { index, user ->
-                ListTileUserItem(
-                    leading = user.picture.medium,
-                    title = "${user.name.first} ${user.name.last}",
-                    subTitle = user.email,
-                ) {
-                    Log.d("ListTileUserItem", "$index. ${user.name.first}")
+        if (isList)
+            LazyColumn {
+                itemsIndexed(items = userResults) { index, user ->
+                    ListTileUserItem(
+                        leading = user.picture.medium,
+                        title = "${user.name.first} ${user.name.last}",
+                        subTitle = user.email,
+                    ) {
+                        Log.d("ListTileUserItem", "$index. ${user.name.first}")
+                    }
                 }
             }
-        }
+        else
+            LazyRow{
+                itemsIndexed(items = userResults) { index, user ->
+                   Text(text = "$index ${user.name.first}")
+                }
+            }
     }
 }
+
 
 @Composable
 fun ListTileUserItem(
