84970d60f5aacc271c5dd1485913f1e942780190
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-10 11:12:12 +0800

    update action item, to add name sorting, and icons

--- app/src/main/java/com/jrektabasa/randomuser/ui/MainActivity.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/MainActivity.kt
@@ -4,19 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -34,11 +29,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.jrektabasa.randomuser.R
 import com.jrektabasa.randomuser.model.UserResult
-import com.jrektabasa.randomuser.ui.components.ActionMenuItem
 import com.jrektabasa.randomuser.ui.components.GridTileUserItem
 import com.jrektabasa.randomuser.ui.components.ListTileUserItem
 import com.jrektabasa.randomuser.ui.screen.viewmodel.GetUserByCountViewModel
@@ -52,7 +47,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                var mDisplayMenu by remember { mutableStateOf(false) }
+                var ascendingOrder by remember { mutableStateOf(true) }
                 var isList by remember { mutableStateOf(true) }
                 Scaffold(
                     topBar = {
@@ -63,37 +58,29 @@ class MainActivity : ComponentActivity() {
                             ),
                             actions = {
                                 IconButton(onClick = {
-                                    mDisplayMenu = !mDisplayMenu
+                                    Log.d("TAG", "onCreate: $ascendingOrder")
+                                    ascendingOrder = !ascendingOrder
                                 }) {
                                     Icon(
-                                        imageVector = Icons.Default.MoreVert,
-                                        contentDescription = null,
+                                        painter = painterResource(
+                                            id = R.drawable.sort_by_alpha_24
+                                        ),
+                                        contentDescription = "sorting by name",
                                         tint = Color.White
                                     )
                                 }
-                                DropdownMenu(
-                                    expanded = mDisplayMenu,
-                                    onDismissRequest = { mDisplayMenu = false },
-                                    modifier = Modifier
-                                        .widthIn(min = 120.dp, max = 240.dp)
-                                        .background(MaterialTheme.colorScheme.background)
-                                ) {
-                                    ActionMenuItem(
-                                        label = "List view", onClick = {
-                                            isList = true
-                                            mDisplayMenu = false
-                                        }
-                                    )
-                                    ActionMenuItem(
-                                        label = "Grid view", onClick = {
-                                            isList = false
-                                            mDisplayMenu = false
-                                        }
-                                    )
-                                    ActionMenuItem(
-                                        label = "Sort by name", onClick = {
-                                            mDisplayMenu = false
-                                        }
+                                IconButton(onClick = {
+                                    isList = !isList
+                                }) {
+                                    Icon(
+                                        painter = if (isList) painterResource(
+                                            id = R.drawable.list_view_24
+                                        )
+                                        else painterResource(
+                                            id = R.drawable.grid_view_24
+                                        ),
+                                        contentDescription = "display view",
+                                        tint = Color.White
                                     )
                                 }
                             }
@@ -105,12 +92,14 @@ class MainActivity : ComponentActivity() {
                                 modifier = Modifier.fillMaxSize(),
                                 color = MaterialTheme.colorScheme.background
                             ) {
-                                RandomUserList(isList = isList)
+                                RandomUserList(
+                                    ascendingOrder = ascendingOrder,
+                                    isList = isList
+                                )
                             }
                         }
                     }
                 )
-
             }
         }
     }
@@ -119,14 +108,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RandomUserList(
     viewModel: GetUserByCountViewModel = hiltViewModel(),
+    ascendingOrder: Boolean = true,
     isList: Boolean = true
 ) {
     val user = viewModel.user.collectAsState()
     if (user.value != null) {
         val userResults: List<UserResult> = user.value!!.results
+        val sortedResult: List<UserResult> = if (ascendingOrder) {
+            userResults.sortedBy { it.name.first }
+        } else {
+            userResults.sortedByDescending { it.name.first }
+        }
         if (isList)
             LazyColumn {
-                itemsIndexed(items = userResults) { index, user ->
+                itemsIndexed(items = sortedResult) { index, user ->
                     ListTileUserItem(
                         icon = user.picture.medium,
                         name = "${user.name.first} ${user.name.last}",
@@ -140,7 +135,7 @@ fun RandomUserList(
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
             ) {
-                itemsIndexed(items = userResults) { index, user ->
+                itemsIndexed(items = sortedResult) { index, user ->
                     GridTileUserItem(
                         icon = user.picture.medium,
                         name = "${user.name.first} ${user.name.last}",
