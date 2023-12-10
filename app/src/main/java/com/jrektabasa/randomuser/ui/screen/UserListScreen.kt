8c8c2fc04436599245ce8f1a5d07c8c1e1ff1d3d
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-10 14:19:55 +0800

    optimize user list screen

--- app/src/main/java/com/jrektabasa/randomuser/ui/screen/UserListScreen.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/screen/UserListScreen.kt
@@ -1,15 +1,12 @@
 package com.jrektabasa.randomuser.ui.screen
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -20,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import com.jrektabasa.randomuser.R
 import com.jrektabasa.randomuser.ui.components.RandomUserList
@@ -37,48 +35,52 @@ fun UserListScreen() {
                     containerColor = MaterialTheme.colorScheme.primary
                 ),
                 actions = {
-                    IconButton(onClick = {
-                        Log.d("TAG", "onCreate: $ascendingOrder")
+                    ActionIconButton(
+                        painter = painterResource(
+                            id = R.drawable.sort_by_alpha_24
+                        ),
+                        description = "sort",
+                    ) {
                         ascendingOrder = !ascendingOrder
-                    }) {
-                        Icon(
-                            painter = painterResource(
-                                id = R.drawable.sort_by_alpha_24
-                            ),
-                            contentDescription = "sorting by name",
-                            tint = Color.White
-                        )
                     }
-                    IconButton(onClick = {
+                    ActionIconButton(
+                        painter = if (isList) painterResource(
+                            id = R.drawable.list_view_24
+                        )
+                        else painterResource(
+                            id = R.drawable.grid_view_24
+                        ),
+                        description = "view",
+                    ) {
                         isList = !isList
-                    }) {
-                        Icon(
-                            painter = if (isList) painterResource(
-                                id = R.drawable.list_view_24
-                            )
-                            else painterResource(
-                                id = R.drawable.grid_view_24
-                            ),
-                            contentDescription = "display view",
-                            tint = Color.White
-                        )
                     }
+
                 }
             )
         },
         content = {
             Box(modifier = Modifier.padding(it)) {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    RandomUserList(
-                        count = 10,
-                        ascendingOrder = ascendingOrder,
-                        isList = isList
-                    )
-                }
+                RandomUserList(
+                    count = 10,
+                    ascendingOrder = ascendingOrder,
+                    isList = isList
+                )
             }
         }
     )
+}
+
+@Composable
+fun ActionIconButton(
+    painter: Painter,
+    description: String = "icon",
+    onToggle: () -> Unit
+) {
+    IconButton(onClick = onToggle) {
+        Icon(
+            painter = painter,
+            contentDescription = description,
+            tint = Color.White
+        )
+    }
 }
