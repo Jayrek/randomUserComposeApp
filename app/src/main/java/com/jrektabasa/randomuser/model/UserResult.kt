e039ce38919862efa1716827183e4f81473b3d13
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-10 21:12:27 +0800

    add UserResult fields (cell, dob, location, nat, phone)

--- app/src/main/java/com/jrektabasa/randomuser/model/UserResult.kt
+++ app/src/main/java/com/jrektabasa/randomuser/model/UserResult.kt
@@ -2,16 +2,16 @@ package com.jrektabasa.randomuser.model
 
 data class UserResult(
     val email: String,
-//    val cell: String,
-//    val dob: Dob,
+    val cell: String,
+    val dob: Dob,
 //    @field:Json(name = "email") val email: String = "",
 //    val gender: String,
 //    val id: Id,
-//    val location: Location,
+    val location: Location,
 //    val login: Login,
     val name: Name,
-//    val nat: String,
-//    val phone: String,
+    val nat: String,
+    val phone: String,
     val picture: Picture,
 //    val registered: Registered
 )
