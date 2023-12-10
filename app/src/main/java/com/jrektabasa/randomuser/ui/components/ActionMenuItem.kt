7a6c418af3e41a198827f8421aedfb8e97a6accd
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-10 11:08:21 +0800

    add condition for selected item

--- app/src/main/java/com/jrektabasa/randomuser/ui/components/ActionMenuItem.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/components/ActionMenuItem.kt
@@ -1,13 +1,20 @@
 package com.jrektabasa.randomuser.ui.components
 
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
-fun ActionMenuItem(label: String, onClick: () -> Unit) {
+fun ActionMenuItem(label: String, isSelected: Boolean, onClick: () -> Unit) {
     DropdownMenuItem(
-        text = { Text(text = label) },
+        text = {
+            Text(
+                text = label,
+                color = if (isSelected) MaterialTheme.colorScheme.primary else Color.Black
+            )
+        },
         onClick = onClick
     )
 }
