08c291c76bceaeeb94c992f15ea640bc019ee2da
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-10 14:18:07 +0800

    add reusable text

--- app/src/main/java/com/jrektabasa/randomuser/ui/components/RandomUserText.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/components/RandomUserText.kt
@@ -8,22 +8,15 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun RandomUserNameText(name: String) {
+fun RandomUserText(
+    label: String,
+    fontSize: Int = 15,
+    color: Color = Color.Black
+) {
     Text(
-        text = name,
-        fontSize = if (name.length >= 12) 15.sp else 18.sp,
-        fontWeight = FontWeight.Bold,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis
-    )
-}
-
-@Composable
-fun RandomUserEmailText(email: String) {
-    Text(
-        text = email,
-        fontSize = 15.sp,
-        color = Color.Gray,
+        text = label,
+        fontSize = fontSize.sp,
+        color = color,
         maxLines = 1,
         overflow = TextOverflow.Ellipsis
     )
