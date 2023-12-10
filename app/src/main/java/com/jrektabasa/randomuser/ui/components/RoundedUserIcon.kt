7b22b2f70bd3b8678a9fc0b223d814d3f10e4dfe
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-12-10 21:50:14 +0800

    add background color to RoundedUserIcon.kt

--- app/src/main/java/com/jrektabasa/randomuser/ui/components/RoundedUserIcon.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/components/RoundedUserIcon.kt
@@ -1,8 +1,10 @@
 package com.jrektabasa.randomuser.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,6 +29,7 @@ fun RoundedUserIcon(
                 width = 1.dp,
                 color = Color.Gray,
                 shape = CircleShape
-            ),
+            )
+            .background(MaterialTheme.colorScheme.primary)
     )
 }
