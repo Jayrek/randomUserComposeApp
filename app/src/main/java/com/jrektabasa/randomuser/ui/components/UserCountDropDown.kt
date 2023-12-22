780f188f73dd702a9fb123709457f6c5ccf583a3
Jayrek <jrektabasa@gmail.com>
2023-12-22 22:29:52 +0800

    remove unused import directives

--- app/src/main/java/com/jrektabasa/randomuser/ui/components/UserCountDropDown.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/components/UserCountDropDown.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.jrektabasa.randomuser.model.userCountList
 import com.jrektabasa.randomuser.ui.screen.viewmodel.GenerateUserViewModel
 
