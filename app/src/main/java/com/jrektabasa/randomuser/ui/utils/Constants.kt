a1acf617d62b9e52dfb40842b183f1affcecb7d2
Jayrek <jrektabasa@gmail.com>
2023-12-22 22:27:53 +0800

    add nationalities on Constants.kt

--- app/src/main/java/com/jrektabasa/randomuser/ui/utils/Constants.kt
+++ app/src/main/java/com/jrektabasa/randomuser/ui/utils/Constants.kt
@@ -3,4 +3,7 @@ package com.jrektabasa.randomuser.ui.utils
 object Constants {
 
     const val BASE_URL = "https://randomuser.me"
+
+    val nationalities = listOf("AU", "US", "BR", "CA", "CH", "DE", "DK")
+    //AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IN, IR, MX, NL, NO, NZ, RS, TR, UA, US
 }
